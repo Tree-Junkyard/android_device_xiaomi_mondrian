bfaabb5623bb3d1bdb869ccf36299de8c04f5ad0
S A U R A V <dwarmachine24@gmail.com>
2024-06-21 17:50:26 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,7 +4,7 @@ git clone --depth=1 https://github.com/Tree-Junkyard/android_device_xiaomi_sm845
 rm -rf vendor/xiaomi
 git clone --depth=1 https://github.com/mondrian-x/vendor_xiaomi_mondrian vendor/xiaomi/mondrian
 git clone --depth=1 https://github.com/mondrian-x/vendor_xiaomi_sm8450-common vendor/xiaomi/sm8450-common
-git clone --depth=1 https://github.com/mondrian-x/vendor_xiaomi_camera vendor/xiaomi/camera
+# git clone --depth=1 https://github.com/mondrian-x/vendor_xiaomi_camera vendor/xiaomi/camera
 
 rm -rf kernel/xiaomi
 git clone --depth=1 https://github.com/cupid-development/android_kernel_xiaomi_sm8450 kernel/xiaomi/sm8450
